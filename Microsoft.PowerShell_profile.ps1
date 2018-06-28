28acad83478aaf4cea60324502f22666ebb4c9be
Ernesto Barros <e.leon.barros@gmail.com>
2018-06-28 13:35:57 +0200

    Added my PowherShell dot file

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+$HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
+Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFilePath } | out-null
+if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
+# if you don't already have this configured...
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+# Import oh-my-posh and dependecies
+Import-Module -Name posh-git -ErrorAction SilentlyContinue
+Import-Module oh-my-posh
+
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+# Neovim
+# Set-Alias vi 'nvim-qt'
+function vi { nvim-qt --maximized $args }
